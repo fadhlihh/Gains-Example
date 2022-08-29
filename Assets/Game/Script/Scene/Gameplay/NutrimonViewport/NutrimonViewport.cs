7d596d33e6e2593ebf53eb0fd2d176779813efdf
fadhlihh <fadhlimorth5@gmail.com>
2022-08-29 17:08:03 +0700

    Add nutrimon info in home, add customize porfile layout, polish background in viewport

--- Assets/Game/Script/Scene/Gameplay/NutrimonViewport/NutrimonViewport.cs
+++ Assets/Game/Script/Scene/Gameplay/NutrimonViewport/NutrimonViewport.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 using Gains.Module.ProgressionData;
+using Gains.Module.WIP;
 
 namespace Gains.Module.Viewport
 {
@@ -9,6 +12,16 @@ namespace Gains.Module.Viewport
     {
         [SerializeField]
         private GameObject _viewportContainer;
+        [SerializeField]
+        private TextMeshProUGUI _nameText;
+        [SerializeField]
+        private TextMeshProUGUI _levelText;
+        [SerializeField]
+        private Button _customizeButton;
+        [SerializeField]
+        private List<Image> _nutrimonStars = new List<Image>();
+        [SerializeField]
+        private WorkInProgress _workInProgressPopUp;
 
         private GameObject _nutrimon;
         private Touch _touch;
@@ -24,7 +37,26 @@ namespace Gains.Module.Viewport
         private void SetNutrimonOnViewport()
         {
             GameObject nutrimonPrefabs = Resources.Load<GameObject>($"Prefabs/Nutrimon/{ProgressData.Instance.Progress.SelectedNutrimon.Name}");
+            _nameText.text = ProgressData.Instance.Progress.SelectedNutrimon.Name;
+            _levelText.text = ProgressData.Instance.Progress.SelectedNutrimon.Level.ToString();
+            CheckNutrimonStar();
             _nutrimon = Instantiate(nutrimonPrefabs, _viewportContainer.transform);
+            _customizeButton.onClick.RemoveAllListeners();
+            _customizeButton.onClick.AddListener(OnCustomize);
+        }
+
+        private void CheckNutrimonStar(){
+            float nutrimonStars = ProgressData.Instance.Progress.NewNutrimon.Star;
+            int index = 0;
+            while (index < Mathf.FloorToInt(nutrimonStars))
+            {
+                _nutrimonStars[index].sprite = Resources.Load<Sprite>(@"Sprites/Stars/stage_character_star");
+                index++;
+            }
+            if ((nutrimonStars % 1) != 0)
+            {
+                _nutrimonStars[index].sprite = Resources.Load<Sprite>(@"Sprites/Stars/stage_character_star_half");
+            }
         }
 
         private void OnMouseDrag()
@@ -39,6 +71,10 @@ namespace Gains.Module.Viewport
                     _viewportContainer.transform.rotation = _rotationY * _viewportContainer.transform.rotation;
                 }
             }
+        }
+
+        private void OnCustomize(){
+            _workInProgressPopUp.Show();
         }
     }
 }
